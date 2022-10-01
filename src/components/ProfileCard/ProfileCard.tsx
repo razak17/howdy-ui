4e2da411a199e61314d6d853d6a3d1425d29410e
Razak <moabdulrazak17@gmail.com>
2022-10-01 21:41:39 +0000

    feat(components): create profile card

--- src/components/ProfileCard/ProfileCard.tsx
+++ src/components/ProfileCard/ProfileCard.tsx
@@ -1,7 +1,39 @@
-const ProfileCard = () => {
-  return (
-    <div>ProfileCard</div>
-  )
-}
+import { Link } from 'react-router-dom';
+import coverImg from '../../assets/cover.jpg';
+import profileImg from '../../assets/profile.jpg';
+import './ProfileCard.css';
 
-export default ProfileCard
+const ProfileCard = () => {
+	return (
+		<div className='profile-card'>
+			<div className='profile-imgs'>
+				<img className='cover-img' src={coverImg} alt='coverImage' />
+				<img className='profile-img' src={profileImg} alt='profileImage' />
+			</div>
+			<div className='profile-name'>
+				<span>Jane Doe</span>
+				<span>Write about yourself</span>
+			</div>
+			<div className='follow-status'>
+				<div>
+					<div className='follow'>
+						<span>4</span>
+						<span>Followers</span>
+					</div>
+					<div className='vl'></div>
+					<div className='follow'>
+						<span>4</span>
+						<span>Following</span>
+					</div>
+				</div>
+			</div>
+			<span>
+				<Link to={`/profile/jane123`} className='profile-link'>
+					My Profile
+				</Link>
+			</span>
+		</div>
+	);
+};
+
+export default ProfileCard;
