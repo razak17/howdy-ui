94a4ed5202a4947bde801e5e65f79746e795333f
Razak <moabdulrazak17@gmail.com>
2022-10-01 20:29:14 +0000

    chore(styles): add some global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { Routes, Route } from 'react-router-dom';
+
 import NotFoundPage from './pages/404/404';
 import Auth from './pages/Auth/Auth';
 import Chat from './pages/Chat/Chat';
 import Home from './pages/Home/Home';
 import Profile from './pages/Profile/Profile';
 
+import './App.css'
+
 const App = () => {
 	return (
-		<div>
+		<div className='container'>
 			<Routes>
 				<Route path='/' element={<Home />} />
 				<Route path='/home' element={<Home />} />
