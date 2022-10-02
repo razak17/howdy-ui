d411a89c3dff94aa23db0fbbbc4d2161d19cae42
Razak <moabdulrazak17@gmail.com>
2022-10-02 02:12:59 +0000

    feat(components): create nav icons

--- src/components/NavIcons/NavIcons.tsx
+++ src/components/NavIcons/NavIcons.tsx
@@ -1,8 +1,23 @@
+import { UilSetting } from '@iconscout/react-unicons';
+import { Link } from 'react-router-dom';
+
+import Home from '../../assets/home.png';
+import Notif from '../../assets/notif.png';
+import Comment from '../../assets/comment.png';
+
 const NavIcons = () => {
-  return (
-    <div>NavIcons</div>
-  )
-}
+	return (
+		<div className='nav-icons'>
+			<Link to='../home'>
+				<img src={Home} alt='home' />
+			</Link>
+			<UilSetting />
+			<img src={Notif} alt='notification' />
+			<Link to='../chat'>
+				<img src={Comment} alt='comment' />
+			</Link>
+		</div>
+	);
+};
 
-export default NavIcons
-
+export default NavIcons;
