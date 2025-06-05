43f26b5f2701fe555ab0547592c7ae8b0923ea29
Abdul Razak <moabdulrazak17@gmail.com>
2025-06-05 22:07:14 +0000

    chore(eslint): config cleanup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,6 +7,7 @@ import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 
 export default [
+	...tseslint.configs.recommended,
 	{ ignores: ['dist'] },
 	{
 		files: ['**/*.{js,jsx,mjs,cjs,ts,tsx}'],
@@ -25,33 +26,32 @@ export default [
 		},
 		rules: {
 			...eslint.configs.recommended.rules,
-			...tseslint.configs.recommended.rules,
 			...react.configs.recommended.rules,
 			...reactHooks.configs.recommended.rules,
 			...react.configs['jsx-runtime'].rules,
-			quotes: [0, 'single'],
-			'no-unused-vars': [1, { args: 'after-used', argsIgnorePattern: '^_' }],
+			quotes: ['off', 'single'],
+			'no-unused-vars': ['warn', { args: 'after-used', argsIgnorePattern: '^_' }],
 			'react-refresh/only-export-components': [
 				'warn',
 				{ allowConstantExport: true }
 			],
 			semi: 1,
 			'react/jsx-no-target-blank': 'off',
-			'no-shadow': 0,
-			'no-console': 0,
-			'linebreak-style': 0,
-			'object-curly-spacing': [1, 'always'],
-			'object-curly-newline': 0,
-			'max-len': [1, { code: 120 }],
-			'react/function-component-definition': 0,
-			'react/prop-types': 0,
-			'react/state-in-constructor': 0,
-			'react/react-in-jsx-scope': 0,
-			'import/extensions': 0,
-			'import/prefer-default-export': 0,
-			'react/jsx-filename-extension': 0,
-			'react/jsx-one-expression-per-line': 0,
-			'@typescript-eslint/no-unused-vars': 0
+			'no-shadow': 'off',
+			'no-console': 'off',
+			'linebreak-style': 'off',
+			'object-curly-spacing': ['warn', 'always'],
+			'object-curly-newline': 'off',
+			'max-len': ['warn', { code: 120 }],
+			'react/function-component-definition': 'off',
+			'react/prop-types': 'off',
+			'react/state-in-constructor': 'off',
+			'react/react-in-jsx-scope': 'off',
+			'import/extensions': 'off',
+			'import/prefer-default-export': 'off',
+			'react/jsx-filename-extension': 'off',
+			'react/jsx-one-expression-per-line': 'off',
+			'@typescript-eslint/no-unused-vars': 'off'
 		}
 	}
 ];
